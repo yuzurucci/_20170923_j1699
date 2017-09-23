27337ae970b1430fee78024f39a39447092be439
yuzurucci <yuccun@gmail.com>
2017-09-24 04:04:45 +0900

    my_subprocess modified.

--- my_subprocess.py
+++ my_subprocess.py
@@ -5,7 +5,7 @@ import time
 cmd = 'j1699.exe'
 
 #the_directory = "C:/MinGW/msys/1.0/home/YuzuruM"
-the_directory = "C:/Users/YuzuruM/Documents/MATLAB/-20170222_sampleIOModelwithCAN_forDT_20170923_j1699
+the_directory = "C:/Users/YuzuruM/Documents/MATLAB/-20170222_sampleIOModelwithCAN_forDT_20170923_j1699"
 p = Popen(cmd.split(), shell=True, stdin=PIPE, cwd = the_directory,stdout=PIPE)
 print p.stdout.readline()
 time.sleep(3)
