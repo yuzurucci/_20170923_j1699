c869a4f1da2f63b6d720c1c3b266f27d003e30ba
yuzurucci <yuccun@gmail.com>
2017-09-24 04:04:06 +0900

    my_subprocess modified.

--- my_subprocess.py
+++ my_subprocess.py
@@ -2,11 +2,12 @@ import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import time
 
-cmd2_2 = 'askbirthmonth2.exe'
+cmd = 'j1699.exe'
 
-the_directory = "C:/MinGW/msys/1.0/home/YuzuruM"
-p = Popen(cmd2_2.split(), shell=True, stdin=PIPE, cwd = the_directory,stdout=PIPE)
+#the_directory = "C:/MinGW/msys/1.0/home/YuzuruM"
+the_directory = "C:/Users/YuzuruM/Documents/MATLAB/-20170222_sampleIOModelwithCAN_forDT_20170923_j1699
+p = Popen(cmd.split(), shell=True, stdin=PIPE, cwd = the_directory,stdout=PIPE)
 print p.stdout.readline()
 time.sleep(3)
-p.stdin.write("10\n")
+p.stdin.write("1\n")
 print p.stdout.readline()
