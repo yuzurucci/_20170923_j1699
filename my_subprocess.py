f1e7a88105a3b7235ec2789b8a7dac4bcedbd727
yuzurucci <yuccun@gmail.com>
2017-09-24 11:23:21 +0900

    add \n in printf command in case YES_NO_PROMPT in LogPrint.c

--- my_subprocess.py
+++ my_subprocess.py
@@ -14,348 +14,123 @@ p = Popen(cmd.split(), shell=True, stdin=PIPE, cwd = the_directory,stdout=PIPE)
 for num in range(1,45):
     current_sentense = p.stdout.readline()
     if_enter = ("Enter" in current_sentense)
-    print current_sentense + "one sentense finished. " + str(len(current_sentense)) + " " + str(if_enter)
+    print current_sentense 
     if if_enter:
             break
-#print "exited from for loop."
 p.stdin.write("1\n")
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
+print "1"
 
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("2000\n")
+print "2000"
 
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("Toyota\n")
+print "Toyota"
 
-"""
-print "found Enter choise."
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("Crown\n")
+print "Crown"   #Q05
 
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-print p.stdout.readline()
-"""
-"""
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-if p.stdout.readline()!="Enter choice (1 or 2): ":
-    print p.stdout.readline()
-else:
-    print "found Enter choise."
-"""
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q06
+print "1"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q07
+print "1"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q08
+print "1"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q09
+print "1 Conventional"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q10
+print "1 US OBD-II"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q11 
+print "1 Light/Medium"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n") #Q13
+print "1 ??"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n")
+print "1"
+
+for num in range(1,20):
+    current_sentense = p.stdout.readline()
+    if_enter = ("Enter" in current_sentense)or("How many" in current_sentense)
+    print current_sentense 
+    if if_enter:
+            break
+p.stdin.write("1\n")
+print "1"
+
+
+
+
 """
 for num in range(1,45):
     if p.stdout.readline()!="Enter choice (1 or 2): ":
